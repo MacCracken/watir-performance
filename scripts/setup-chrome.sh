ec18b7baffa1362a0c793358cf13a8bddcc92601
Robert MacCracken <robert.maccracken@gmail.com>
2018-05-04 14:19:11 -0700

    Update setup-chrome.sh

--- scripts/setup-chrome.sh
+++ scripts/setup-chrome.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-echo "#### Getting Chrome ####"
+#echo "#### Getting Chrome ####"
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
 sudo apt-get update 

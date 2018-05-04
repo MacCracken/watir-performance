5c4fae07e0a00437264f2ce8c0cc03f03543d351
Robert MacCracken <robert.maccracken@gmail.com>
2018-05-04 16:18:25 -0700

    Update setup-chromedriver.sh

--- scripts/setup-chromedriver.sh
+++ scripts/setup-chromedriver.sh
@@ -3,9 +3,8 @@ echo "#### Getting Chromedriver ####"
 latest_chromedriver=$(wget -qO- http://chromedriver.storage.googleapis.com/LATEST_RELEASE)
 wget https://chromedriver.storage.googleapis.com/${latest_chromedriver}/chromedriver_linux64.zip
 unzip chromedriver_linux64.zip
-sudo cp chromedriver /usr/bin/chromedriver
-sudo chown root /usr/bin/chromedriver
-sudo chmod +x /usr/bin/chromedriver
-sudo chmod 755 /usr/bin/chromedriver
+sudo cp chromedriver /usr/local/bin/chromedriver
+sudo chown root /usr/local/bin/chromedriver
+sudo chmod +x /usr/local/bin/chromedriver
+sudo chmod 755 /usr/local/bin/chromedriver
 rm chromedriver*
-sudo apt-get autoremove -y

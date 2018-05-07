cb6f8e1f69d7e3419758fe3dd2b1529ba896e12f
maccracken <robert.maccracken@gmail.com>
2018-05-07 13:05:21 -0700

    trying something else

--- spec/support/setup-geckodriver-alt.sh
+++ spec/support/setup-geckodriver-alt.sh
@@ -4,4 +4,3 @@ echo "Using GeckoDriver version: "$GK_VERSION
 wget --no-verbose -O /tmp/geckodriver.tar.gz https://github.com/mozilla/geckodriver/releases/download/v$GK_VERSION/geckodriver-v$GK_VERSION-linux64.tar.gz
 tar -C /opt -zxf /tmp/geckodriver.tar.gz && rm /tmp/geckodriver.tar.gz 
 sudo chmod 755 /opt/geckodriver
-export PATH=$PATH:/opt/geckodriver

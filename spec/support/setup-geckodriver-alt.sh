6596c7278f8f36be634ec26cffc09c76b01b09b7
maccracken <robert.maccracken@gmail.com>
2018-11-14 09:43:48 -0800

    fixes to geckodriver install

--- spec/support/setup-geckodriver-alt.sh
+++ spec/support/setup-geckodriver-alt.sh
@@ -1,6 +1,5 @@
-echo "#### Getting Geckodriver ####"
-GK_VERSION=0.20.1
-echo "Using GeckoDriver version: "$GK_VERSION 
+GK_VERSION=0.23.0
+echo "#### Getting Geckodriver version: " $GK_VERSION
 wget --no-verbose -O /tmp/geckodriver.tar.gz https://github.com/mozilla/geckodriver/releases/download/v$GK_VERSION/geckodriver-v$GK_VERSION-linux64.tar.gz
 tar -C /opt -zxf /tmp/geckodriver.tar.gz && rm /tmp/geckodriver.tar.gz 
 sudo chmod 755 /opt/geckodriver

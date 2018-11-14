751e39c931130ef7e333409a091287bb4c53f2dc
maccracken <robert.maccracken@gmail.com>
2018-11-14 11:00:05 -0800

    trying again to fix firefox testing

--- spec/support/setup-geckodriver-alt.sh
+++ spec/support/setup-geckodriver-alt.sh
@@ -1,7 +1,6 @@
 GK_VERSION=0.23.0
 echo "#### Getting Geckodriver version: " $GK_VERSION
 wget --no-verbose -O /tmp/geckodriver.tar.gz https://github.com/mozilla/geckodriver/releases/download/v$GK_VERSION/geckodriver-v$GK_VERSION-linux64.tar.gz
-tar -C /opt -zxf /tmp/geckodriver.tar.gz && rm /tmp/geckodriver.tar.gz 
-sudo chmod 755 /opt/geckodriver
-sudo ln -fs /opt/geckodriver-$GK_VERSION /usr/local/bin/geckodriver
-export PATH="$PATH:/user/local/bin/geckodriver"
+tar -C ~/bin -zxf /tmp/geckodriver.tar.gz && rm /tmp/geckodriver.tar.gz 
+sudo chmod 755 ~/bin/geckodriver
+export PATH=~/bin:$PATH
--- spec/support/setup-geckodriver.sh
+++ spec/support/setup-geckodriver.sh
@@ -3,6 +3,6 @@ GK_VERSION=$(if [ ${GECKODRIVER_VERSION:-latest} = "latest" ]; then echo $(wget 
 echo "#### Getting Geckodriver version: " $GK_VERSION
 wget --no-verbose -O /tmp/geckodriver.tar.gz https://github.com/mozilla/geckodriver/releases/download/v$GK_VERSION/geckodriver-v$GK_VERSION-linux64.tar.gz
 sudo tar -C /opt -zxf /tmp/geckodriver.tar.gz && rm /tmp/geckodriver.tar.gz 
-mv /opt/geckodriver /opt/geckodriver-$GK_VERSION 
+sudo mv /opt/geckodriver /opt/geckodriver-$GK_VERSION 
 sudo chmod 755 /opt/geckodriver-$GK_VERSION 
 sudo ln -fs /opt/geckodriver-$GK_VERSION /usr/local/bin/geckodriver

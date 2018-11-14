0c38836df80765e476109072af48fd1df42310db
maccracken <robert.maccracken@gmail.com>
2018-11-14 10:05:44 -0800

    second attempt to repair geckodriver

--- spec/support/setup-geckodriver-alt.sh
+++ spec/support/setup-geckodriver-alt.sh
@@ -4,3 +4,4 @@ wget --no-verbose -O /tmp/geckodriver.tar.gz https://github.com/mozilla/geckodri
 tar -C /opt -zxf /tmp/geckodriver.tar.gz && rm /tmp/geckodriver.tar.gz 
 sudo chmod 755 /opt/geckodriver
 sudo ln -fs /opt/geckodriver-$GK_VERSION /usr/local/bin/geckodriver
+PATH=$PATH:/user/local/bin
